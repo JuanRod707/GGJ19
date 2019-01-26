a4a9354c9ddd72706bbaccbc2be2ecafca1977cb
kari89diaz <kari89diaz@gmail.com>
2019-01-26 17:26:29 -0300

    Objetos de la casa ubicados

---
+++ Assets/Scripts/Effects/PlayAnimation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Effects
+{
+    public class PlayAnimation : SpookEffect
+    {
+        public Animator Controller;
+
+        public override void PlayEffect() => Controller.SetTrigger("Spook");
+    }
+}
