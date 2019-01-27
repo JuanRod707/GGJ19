cb7489c4820cb479ecae88e299a2a91ca82fdf05
Juancho <juan.rod707@gmail.com>
2019-01-27 12:47:45 -0300

    Minimap

---
+++ Assets/Scripts/Effects/FreezeRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Vector3 = System.Numerics.Vector3;
+
+namespace Effects
+{
+    public class FreezeRotation : MonoBehaviour
+    {
+        void Update()
+        {
+            var rotVector = transform.eulerAngles;
+            rotVector.y = 0;
+            transform.eulerAngles = rotVector;
+        }
+    }
+}
