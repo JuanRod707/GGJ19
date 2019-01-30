d2e3298a0290a60fc1f4dd343c1bc374995acc5c
Juancho <juan.rod707@gmail.com>
2019-01-30 00:14:19 -0300

    escape hace cosas

---
+++ Assets/Scripts/UI/KeyboardNavigation.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class KeyboardNavigation : MonoBehaviour
+    {
+        public NavigationAction EscapeAction;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                switch (EscapeAction)
+                {
+                    case NavigationAction.GoToMenu:
+                        SceneManager.LoadScene("MainMenu");
+                        break;
+                    case NavigationAction.Quit:
+                        Application.Quit();
+                        break;
+                }
+            }
+        }
+    }
+
+    public enum NavigationAction
+    {
+        GoToMenu,
+        Quit
+    }
+}
