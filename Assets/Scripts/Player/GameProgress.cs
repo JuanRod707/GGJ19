375055c67de18f5842c41e149b4403d5f9f59995
Juancho <juan.rod707@gmail.com>
2019-01-29 23:54:21 -0300

    tutorial

--- Assets/Scripts/Player/GameProgress.cs
+++ Assets/Scripts/Player/GameProgress.cs
@@ -24,7 +24,7 @@ namespace Player
         public void VisitorSpooked()
         {
             spookedVisitors++;
-            UiController.AddScore();
+            //UiController.AddScore();
             if (spookedVisitors >= visitorCount)
                 WinGame();
         }
